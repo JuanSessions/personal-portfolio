b457a3cb841b6838007e3028ad837022ff3ee203
JuanSessions <juanperusin@yahoo.es>
2020-06-08 16:22:17 +0200

    header style

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <h4>Juan</h4>
+          <h4 className="juan">Juan</h4>
           <div className="trigger">
 
             <img src={logo} className="App-logo" alt="logo" />
