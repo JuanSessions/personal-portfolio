5fe0120f38abc3fa3f4af7ff2489a8c2f03ddad5
JuanSessions <juanperusin@yahoo.es>
2020-07-02 16:52:03 +0200

    change position

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Countdown from "./Countdown"
 import img1 from "../images/musician-portfolio.png"
 import img2 from "../images/todoapp.png"
-import img3 from "../images/bookShop.png"
+import img3 from "../images/ScreenshotVMR.png"
 import useSound from "use-sound";
 import ImgSound from "../sounds/cameraClick.mp3";
 
@@ -19,11 +19,18 @@ function Projects() {
                 <h2>Projects</h2>
 
                 <div className="projects-container">
+                    <div className="project">
+                        <h3>Virtual Music Room</h3>
+                        <a href="http://161.35.217.162" target="_blanc">
+                            <img src={img3} alt="" width="260" onClick={playActive} />
+                        </a>
+                        <h3>Final project full stack</h3>
+                    </div>
 
                     <div className="project">
                         <h3>Musician Portfolio</h3>
                         <a href="https://juansessions.github.io/musician-portfolio/" target="_blanc">
-                            <img src={img1} alt="" width="250" onClick={playActive} />
+                            <img src={img1} alt="" width="200" onClick={playActive} />
                         </a>
                     </div>
 
@@ -33,16 +40,6 @@ function Projects() {
                         <a href="https://juansessions.github.io/TODO-APP/#/" target="_blanc">
                             <img src={img2} alt="" width="150" onClick={playActive} />
                         </a>
-                    </div>
-
-
-                    <div className="project">
-                        <h3>On Line Book Shop</h3>
-
-                        <a href="https://juansessions.github.io/mongo-contacts/" target="_blanc">
-                            <img src={img3} alt="" width="245" onClick={playActive} />
-                        </a>
-                        <p>Basic Full Stack - Login And Register</p>
                     </div>
 
                 </div>
