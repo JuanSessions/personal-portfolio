282b0ae09d1431743c660bbb1d4ab105bf37ed9d
JuanSessions <juanperusin@yahoo.es>
2020-07-02 12:47:00 +0200

    build out of gitignore

---
+++ build/precache-manifest.8f6a659d360bf4543a771bd710f904a4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11072d00953984a2ab7d231f1df157ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bca57c9ed83cbf6b366a",
+    "url": "/static/css/main.c316bd1b.chunk.css"
+  },
+  {
+    "revision": "9c95fddb30bad715bee2",
+    "url": "/static/js/2.ccf31b78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ccf31b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581955222309f28d86a5",
+    "url": "/static/js/3.7b82a7a9.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/static/js/3.7b82a7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca57c9ed83cbf6b366a",
+    "url": "/static/js/main.b933cc85.chunk.js"
+  },
+  {
+    "revision": "150c0ae4c09ae5e5a6ce",
+    "url": "/static/js/runtime-main.b7277a44.js"
+  },
+  {
+    "revision": "7a8991a58173dccc5ec1ecef319e2025",
+    "url": "/static/media/bookShop.7a8991a5.png"
+  },
+  {
+    "revision": "83239ad2ef5220ad267479cd0898e58c",
+    "url": "/static/media/cameraClick.83239ad2.mp3"
+  },
+  {
+    "revision": "814126ccdcbf7708b4195f493b9b65c2",
+    "url": "/static/media/film_grain-small.814126cc.gif"
+  },
+  {
+    "revision": "b8cdd0ec340e161bb3fb5cc8daa0b0d1",
+    "url": "/static/media/flyby.b8cdd0ec.mp3"
+  },
+  {
+    "revision": "cfee114b0b3de28827b98bee2ac0d6f0",
+    "url": "/static/media/icons8-mongodb.cfee114b.svg"
+  },
+  {
+    "revision": "2b23193fe60c798e73b8a8c688d03fee",
+    "url": "/static/media/juanEar.2b23193f.jpg"
+  },
+  {
+    "revision": "2dd8edafb3450c91e18b7b4d7843940b",
+    "url": "/static/media/musician-portfolio.2dd8edaf.png"
+  },
+  {
+    "revision": "fe7a1614ff181af8ee44f81a52d41815",
+    "url": "/static/media/portfolio-mask.fe7a1614.jpg"
+  },
+  {
+    "revision": "341659acc33a1000c7295409018abc3f",
+    "url": "/static/media/soundTrack.341659ac.wav"
+  },
+  {
+    "revision": "ed093146ef132fcf22f1b5fa296c3d60",
+    "url": "/static/media/templeBell.ed093146.mp3"
+  },
+  {
+    "revision": "22dd50726d656ec447840b21f892aaf1",
+    "url": "/static/media/todoapp.22dd5072.png"
+  }
+]);
